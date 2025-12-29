3fd67810cbc3027007595a5de6990c35ba598e64
rizkyberutu <rizkyy0103@gmail.com>
2025-12-29 11:49:22 +0700

    initial project

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="root">
         {children}
+      </div>
       </body>
     </html>
   );
